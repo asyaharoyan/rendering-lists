df23f2b7c4121e12b2cf1e3bb14b8297015a865a
Asya Haroyan <alharoyan@gmail.com>
2024-07-08 07:23:43 +0000

    Search handler

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,15 +9,27 @@ class Content extends Component {
     constructor(props){
         super(props);
         this.state = {
-          isLoaded: false
+          isLoaded: false,
+          posts: []
         }
     }
     componentDidMount() {
       setTimeout(() => {
         this.setState({
           isLoaded: true,
+          posts: savedPosts,
         })
       }, 2000)
+    }
+
+    searchHandler = (e) => {
+      const name = e.target.value.toLowerCase();
+      const filteredPosts = savedPosts.filter((post) => {
+        return post.name.toLowerCase().includes(name);
+      });
+      this.setState({
+        posts: filteredPosts
+      });
     }
 
   render() {
@@ -25,6 +37,16 @@ class Content extends Component {
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label htmlFor='searchinput'>Search</label>
+              <input
+                type='search'
+                id='searchinput'
+                placeholder='By Author'
+                onChange={(e) => this.searchHandler(e)}
+                />
+              <h4>Posts found: {this.state.posts.length}</h4>
+            </form>
         </div>
         <div className={css.SearchResults}>
           {
