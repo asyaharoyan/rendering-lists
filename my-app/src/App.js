03a6ed25cf35f2c59e68a219a8ddfede1dbfcd5a
Asya Haroyan <alharoyan@gmail.com>
2024-07-18 09:20:43 +0000

    Add API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 // import Content from './components/Content';
-import ContentHooks from './components/ContentHooks';
+// import ContentHooks from './components/ContentHooks';
+import ContentAPI from './components/ContentAPI';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <ContentHooks />
+      <ContentAPI />
       <Sidebar />
     </div>
   );
